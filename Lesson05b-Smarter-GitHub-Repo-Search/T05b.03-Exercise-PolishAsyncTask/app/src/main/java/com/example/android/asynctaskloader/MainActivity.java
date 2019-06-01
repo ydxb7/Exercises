108801cbefa25ab44a1be7372fe53622a5b4a272
Yini <ydxb7@mail.missouri.edu>
2019-05-31 17:27:49 -0700

    T05b.03 缓存加载器结果，防止客户离开应用后出现查询

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -166,6 +166,7 @@ public class MainActivity extends AppCompatActivity implements
         return new AsyncTaskLoader<String>(this) {
 
             // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
+            private String mGithubJson;
 
             @Override
             protected void onStartLoading() {
@@ -176,14 +177,15 @@ public class MainActivity extends AppCompatActivity implements
                 }
 
                 // TODO (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
-
-                /*
+                if(mGithubJson != null){/*
                  * When we initially begin loading in the background, we want to display the
                  * loading indicator to the user
                  */
-                mLoadingIndicator.setVisibility(View.VISIBLE);
-
-                forceLoad();
+                    deliverResult(mGithubJson);
+                } else{
+                    mLoadingIndicator.setVisibility(View.VISIBLE);
+                    forceLoad();
+                }
             }
 
             @Override
@@ -210,6 +212,11 @@ public class MainActivity extends AppCompatActivity implements
 
             // TODO (3) Override deliverResult and store the data in mGithubJson
             // TODO (4) Call super.deliverResult after storing the data
+            @Override
+            public void deliverResult(String data) {
+                mGithubJson = data;
+                super.deliverResult(data);
+            }
         };
     }
 

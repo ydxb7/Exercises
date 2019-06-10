63bfc8015d6c9e4acaa2d2adbfae44276aafea78
Yini <ydxb7@mail.missouri.edu>
2019-06-09 22:22:45 -0700

    update

---
+++ Advanced-Lesson02-Emojify-LIBRARY/03-Exercise-GetClassificationProbabilities/app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,99 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
+import java.util.List;
+
+class Emojifier {
+
+    private static final String LOG_TAG = Emojifier.class.getSimpleName();
+
+    /**
+     * Method for detecting faces in a bitmap.
+     *
+     * @param context The application context.
+     * @param picture The picture in which to detect the faces.
+     */
+    public static void detectFaces(final Context context, Bitmap picture){
+
+        // Configure the face detector
+        FirebaseVisionFaceDetectorOptions highAccuracyOpts =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .build();
+
+        // Get an instance of FirebaseVisionFaceDetector:
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                .getVisionFaceDetector(highAccuracyOpts);
+
+        // Create a FirebaseVisionImage object from your image.
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(picture);
+
+        // pass the image to the detectInImage method
+        Task<List<FirebaseVisionFace>> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionFace> faces) {
+                                        // Task completed successfully
+                                        // ...
+                                        Log.d(LOG_TAG, "detectFaces: number of faces = " + faces.size());
+
+                                        if(faces.size() == 0){
+                                            Toast.makeText(context, "No Faces Detected", Toast.LENGTH_SHORT).show();
+                                        }
+
+
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        Log.d(LOG_TAG, "Failed to detect the faces.");
+                                    }
+                                });
+
+        // TODO (2): Iterate through the faces, calling getClassifications() for each face.
+
+    }
+
+
+    // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+}

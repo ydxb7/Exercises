bc3560519f32432cf6613e2466e1d69feb3c97db
Yini <ydxb7@mail.missouri.edu>
2019-06-14 03:56:58 -0700

    Advanced06.02 在小部件上创建浇水按钮，该按钮会启动 IntentService 以更新花园中所有植物的浇水时间 -AddWateringService

---
+++ Advanced-Lesson06-MyGarden-Widget/02-Exercise-AddWateringService/app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -0,0 +1,74 @@
+package com.example.android.mygarden;
+
+import android.app.IntentService;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.utils.PlantUtils;
+
+import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
+import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+
+//TODO (2): Create a plant watering service that extends IntentService and supports the
+// action ACTION_WATER_PLANTS which updates last_watered timestamp for all plants still alive
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ */
+public class PlantWateringService extends IntentService {
+
+    public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
+
+    public PlantWateringService() {
+        super("PlantWateringService");
+    }
+
+    /**
+     * Starts this service to perform WaterPlants action with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionWaterPlants(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+
+    /**
+     * @param intent
+     */
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_WATER_PLANTS.equals(action)) {
+                handleActionWaterPlants();
+            }
+        }
+    }
+
+    /**
+     * Handle action WaterPlant in the provided background thread with the provided
+     * parameters.
+     */
+    // 要给所有植物浇水，我们只需运行更新查询，将上次浇水时间设为现在，但仅针对还存活的植物。
+    // 要检查某棵植物是否还存活着，你可以将上次浇水时间与现在进行对比，如果差大于 MAX_AGE_WITHOUT_WATER，那么该植物已经死了！
+    private void handleActionWaterPlants() {
+        Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        ContentValues contentValues = new ContentValues();
+        long timeNow = System.currentTimeMillis();
+        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
+        // Update only plants that are still alive
+        getContentResolver().update(
+                PLANTS_URI,
+                contentValues,
+                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME+">?",
+                new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)});
+    }
+}
+
